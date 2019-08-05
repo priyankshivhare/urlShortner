2ec26bd116b77353735861cfd2aa22f0396e144d
pshivhare <priyank.shivhare@infobip.com>
2019-08-05 12:18:09 +0530

    Added response entity for custom HTTP response

--- src/main/java/com/urlshortner/urlshortner/com/urlshortner/controller/AccountRegistrationController.java
+++ src/main/java/com/urlshortner/urlshortner/com/urlshortner/controller/AccountRegistrationController.java
@@ -3,6 +3,8 @@ package com.urlshortner.urlshortner.com.urlshortner.controller;
 import com.urlshortner.urlshortner.com.urlshortner.model.account.Account;
 import com.urlshortner.urlshortner.com.urlshortner.model.url.UrlData;
 import com.urlshortner.urlshortner.com.urlshortner.model.url.UrlRegistration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -13,30 +15,30 @@ public class AccountRegistrationController {
     private ArrayList<Account> accounts = new ArrayList<>();
 
     @PostMapping(path = "/account")
-    public String accountInformation(@RequestBody Account account) {
+    public ResponseEntity<String> accountInformation(@RequestBody Account account) {
         for (Account storedAccount : accounts) {
             if (storedAccount.getAccountId() == account.getAccountId()) {
-                return "Account already exists";
+                return new ResponseEntity<>("Account already exists", HttpStatus.FOUND);
             }
         }
         Account newAccount = new Account(account.getAccountId(), account.getAccountPassword());
         accounts.add(newAccount);
 
-        return "Your account is opened, your password is: " + newAccount.getAccountPassword();
+        return new ResponseEntity<>("Your account is opened, your password is: " + newAccount.getAccountPassword(), HttpStatus.OK);
     }
 
     @PostMapping(path = "/register")
-    public String urlInformation(@RequestHeader("Authorization") String authorization, @RequestBody UrlRegistration urlData) {
+    public ResponseEntity<String> urlInformation(@RequestHeader("Authorization") String authorization, @RequestBody UrlRegistration urlData) {
         for (Account storedAccount: accounts) {
             if (storedAccount.getAccountId() == urlData.getAccountId()) {
                 if (authorization.equals(storedAccount.getAccountPassword())) {
                     UrlData urlDataObj = new UrlData(urlData.getUrl());
                     storedAccount.setUrlData(urlDataObj);
-                    return "Short Url: " + urlDataObj.getShortUrl() + " Long Url: " + urlDataObj.getLongUrl();
+                    return new ResponseEntity<>("Short Url: " + urlDataObj.getShortUrl() + " Long Url: " + urlDataObj.getLongUrl(), HttpStatus.OK);
                 }
             }
         }
-        return "Authorization failed!";
+        return new ResponseEntity<>("Authorization failed!", HttpStatus.UNAUTHORIZED);
     }
 
     @GetMapping("/{shortUrl}")
@@ -59,12 +61,12 @@ public class AccountRegistrationController {
 
     @GetMapping("/statistic/{accountId}")
     @ResponseBody
-    public String urlRedirect(@PathVariable int accountId) {
+    public ResponseEntity<String> urlRedirect(@PathVariable int accountId) {
         for (Account account: accounts) {
             if (account.getAccountId() == accountId) {
-                return account.getAccountStatistics();
+                return new ResponseEntity<>(account.getAccountStatistics(), HttpStatus.OK);
             }
         }
-        return "No match found!";
+        return new ResponseEntity<>("Account not found", HttpStatus.NOT_FOUND);
     }
 }
